8015f8849aa69234a2df2f539894b916106b95d0
Crefix <57640971+Crefix@users.noreply.github.com>
2020-05-18 14:11:36 +0200

    Factura final

---
+++ Factura.java
@@ -0,0 +1,22 @@
+public class Factura implements IFactura{
+
+	public int num;
+	public float base;
+	public float tipoIva;
+	public Estados estado;
+
+	public void static Main(String[] args) {
+
+	}
+
+	public void cambiarEstado(Estados estado) {
+
+	}
+
+	public float getTotal() {
+
+	}
+
+	public float getCantidadIva() {
+
+	}
