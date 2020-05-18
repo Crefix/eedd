8696a577c175892fb43d5fd870b945d5af31c163
freire97 <64587514+freire97@users.noreply.github.com>
2020-05-18 14:10:51 +0200

    Implementar getTotal, getCantidadIVA()

---
+++ Implementar.java
@@ -0,0 +1,14 @@
+public void cambiarEstado(Estados estado){}
+	public float getTotal(){
+	float numero;
+	return numero;
+	float total;
+	return total;
+	}
+	public float getCantidadIva(){
+	float numero;
+	return numero;
+	float IVA;
+	return IVA;
+	}
+}
